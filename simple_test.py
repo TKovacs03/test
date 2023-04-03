4d14b830c15b6cea3c7a4b663af7b17fc85fb492
TJ Kovacs <tkovacs@terpmail.umd.edu>
2023-04-02 20:47:11 -0400

    added some garbage

---
+++ simple_test.py
@@ -0,0 +1,11 @@
+"""this is a big ol test file so I can figure out how git and github works"""
+
+
+
+class Thing:
+    '''a nondescript class that doesn't really do anything
+    
+    Attributes:
+        number (int): random number
+        letters (str): a bunch of random letters 
+    '''
